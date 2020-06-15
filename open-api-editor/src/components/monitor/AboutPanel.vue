b016bf155f9e990c6e9f920c6d699539395cb8a5
theckert <thomas.james.eckert@gmail.com>
2020-06-14 21:59:09 -0700

    Add monitor components

---
+++ open-api-editor/src/components/monitor/AboutPanel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-100 p-4 rounded mb-2">
+    <button>Donate</button>
+    <button>About</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "AboutPanel"
+});
+</script>
