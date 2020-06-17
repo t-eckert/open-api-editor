cef7fbfcedda1422535dd24939a0da0b3ac89ddf
theckert <thomas.james.eckert@gmail.com>
2020-06-17 00:06:05 -0700

    Add donate feature flag and short description

--- open-api-editor/src/components/monitor/AboutPanel.vue
+++ open-api-editor/src/components/monitor/AboutPanel.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="bg-gray-100 p-4 rounded mb-2">
-    <button>Donate</button>
-    <button>About</button>
+    <div v-if="isExpanded" class="mb-4">
+      <p>A project by Thomas Eckert.</p>
+      <p>Version {{ version }} released {{ released }}</p>
+      <a>Source code</a>
+    </div>
+    <button v-if="isDonatable">Donate</button>
+    <button
+      @click="isExpanded = !isExpanded"
+      class="px-4 py-1 font-medium bg-blue-200 rounded-sm border-gray-800"
+    >
+      About
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { version, released } from "@/main";
+
 export default Vue.extend({
-  name: "AboutPanel"
+  name: "AboutPanel",
+
+  data() {
+    return {
+      isDonatable: false,
+      isExpanded: false,
+      version,
+      released,
+    };
+  },
 });
 </script>
