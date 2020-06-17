0abe1b2b93fcc62be8740906527ff80fef06798f
theckert <thomas.james.eckert@gmail.com>
2020-06-17 00:06:51 -0700

    Show suggestions in UI

--- open-api-editor/src/components/ThePalette.vue
+++ open-api-editor/src/components/ThePalette.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="bg-gray-100 p-4 rounded">
     <h1>The Palette</h1>
+    <pre>{{ suggestions }}</pre>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { mapGetters } from "vuex";
 
 export default Vue.extend({
-  name: "ThePalette"
+  name: "ThePalette",
+
+  computed: mapGetters(["suggestions"]),
 });
 </script>
