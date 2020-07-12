9fb65b8968d63243c15c55a80301b19d240407f5
theckert <thomas.james.eckert@gmail.com>
2020-07-12 13:22:19 -0700

    Suggestions for openapi version

---
+++ open-api-engine/src/enums/Priority.ts
@@ -0,0 +1,10 @@
+/** `Priority` enum
+ *
+ * Represents the state of a suggestion as Required or Optional.
+ */
+enum Priority {
+  Required = "REQUIRED",
+  Optional = "OPTIONAL",
+}
+
+export default Priority;
---
+++ open-api-engine/src/interfaces/Suggestion.ts
@@ -0,0 +1,5 @@
+/** `Suggestion` interface
+ *
+ * Represents an option for editing the Open API document.
+ */
+export default interface Suggestion {}
