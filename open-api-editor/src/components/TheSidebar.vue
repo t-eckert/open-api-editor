948dab6543d737cbd00feebf83f81b33f178abd2
theckert <thomas.james.eckert@gmail.com>
2020-07-16 20:21:41 -0700

    Add TheSidebar

---
+++ open-api-editor/src/components/TheSidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sidebar sidebar__layout">
+    <div class="sidebar__header">
+      <h1>Open API Editor</h1>
+      <button>Hide</button>
+    </div>
+    <OpenApiVersionEditor />
+    <InfoEditor />
+    <ContactEditor />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import OpenApiVersionEditor from "@/components/sidebar/OpenApiVersionEditor.vue";
+import InfoEditor from "@/components/sidebar/InfoEditor.vue";
+import ContactEditor from "@/components/sidebar/ContactEditor.vue";
+import { mapGetters } from "vuex";
+
+export default Vue.extend({
+  name: "TheSidebar",
+
+  components: {
+    OpenApiVersionEditor,
+    InfoEditor,
+    ContactEditor
+  }
+});
+</script>
