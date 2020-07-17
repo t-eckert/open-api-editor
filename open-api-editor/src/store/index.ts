31c892a614ed4a9e9dcadd7dde8b986b1dea7958
theckert <thomas.james.eckert@gmail.com>
2020-07-16 20:20:53 -0700

    Reformat the store

--- open-api-editor/src/store/index.ts
+++ open-api-editor/src/store/index.ts
@@ -1,61 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import yaml from "js-yaml";
-import axios from "axios";
-import PaletteSuggestor from "@/paletteSuggestor";
-
-const paletteSuggestor = new PaletteSuggestor({});
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    doc: "",
-    suggestions: [],
-    errors: [],
-    warnings: [],
-  },
-  getters: {
-    yamlDoc: (state) => {
-      const a = yaml.safeDump(state.doc);
-      return a;
+    doc: {
+      openapi: "3.0.3",
+      info: {
+        title: "",
+      },
     },
-    jsonDoc: (state) => yaml.safeLoad(state.doc),
-    suggestions: (state) => state.suggestions,
   },
+  getters: {},
   mutations: {
     SET_DOC: (state, doc) => {
       state.doc = doc;
     },
-    SET_SUGGESTIONS: (state, suggestions) => {
-      state.suggestions = suggestions;
-    },
-    SET_ERRORS: (state, errors) => {
-      state.errors = errors;
-    },
-    SET_WARNINGS: (state, warnings) => {
-      state.warnings = warnings;
+    SET_TITLE: (state, title) => {
+      state.doc.info.title = title;
     },
   },
-  actions: {
-    updateSuggestions: async (context) => {
-      const suggestions = paletteSuggestor
-        .updateDoc(context.getters["jsonDoc"])
-        .suggest();
-      context.commit("SET_SUGGESTIONS", suggestions);
-    },
-    updateMonitor: async (context) => {
-      const reponse = await axios.post(
-        "http://localhost:7071/api/validate-open-api-doc",
-        context.getters["jsonDoc"],
-        {
-          headers: {
-            "content-type": "application/json",
-            origin: "http://localhost:8080/",
-          },
-        }
-      );
-      context.commit("SET_ERRORS", reponse.data);
-    },
-  },
+  actions: {},
 });
