7b4b13a67bca102aba74623632ff3fb8b17f6104
theckert <thomas.james.eckert@gmail.com>
2020-07-16 20:14:41 -0700

    Re-add tailwind

--- open-api-editor/src/main.ts
+++ open-api-editor/src/main.ts
@@ -2,13 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import "./assets/tailwind.css";
 import "./assets/styles.css";
 
 Vue.config.productionTip = false;
 
-// todo 2020-06-14 t-eckert set these from package.json using Bable at build time.
 export const version = "0.1.0";
-export const released = "14 June 2020";
 
 new Vue({
   store,
