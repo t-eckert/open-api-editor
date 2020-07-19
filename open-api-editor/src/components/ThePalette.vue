f7e931587976bc745f4188a2657317f1c5eb32b3
theckert <thomas.james.eckert@gmail.com>
2020-07-18 21:09:14 -0700

    Unwrap doc for Palette

--- open-api-editor/src/components/ThePalette.vue
+++ open-api-editor/src/components/ThePalette.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="palette__layout">
-    <h1 class="display-1">{{ doc.info.title || "Welcome to your API" }}</h1>
+    <h1 class="display-1">{{ info.title || "Welcome to your API" }}</h1>
     <div class="palette__servers__section">
-      <h1>Servers</h1>
+      <h1 class="text-secondary">Servers</h1>
       <div class="palette__servers__editor-cards">
         <ServerEditorCard />
       </div>
       <button>Add server</button>
     </div>
     <div class="palette__paths__section">
-      <h1>Paths</h1>
+      <h1 class="text-secondary">Paths</h1>
       <div class="palette__paths__editor-cards">
-        <PathEditorCard />
+        <PathEditorCard v-for="path in paths" :key="path.id" :id="path.id" />
       </div>
-      <button>Add path</button>
+      <button @click="createPath()">Add path</button>
     </div>
   </div>
 </template>
@@ -22,13 +22,19 @@
 import Vue from "vue";
 import ServerEditorCard from "@/components/palette/ServerEditorCard.vue";
 import PathEditorCard from "@/components/palette/PathEditorCard.vue";
-import { mapState } from "vuex";
+import { mapState, mapGetters, mapMutations } from "vuex";
 
 export default Vue.extend({
   name: "ThePalette",
 
   components: { ServerEditorCard, PathEditorCard },
 
-  computed: mapState(["doc"]),
+  computed: { ...mapState(["info", "servers", "paths"]) },
+
+  methods: {
+    createPath() {
+      this.$store.commit("CREATE_PATH");
+    },
+  },
 });
 </script>
