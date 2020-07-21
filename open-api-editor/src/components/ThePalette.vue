7ea75467d24aa1bc94ff55f4e76e93f5d7f3be71
theckert <thomas.james.eckert@gmail.com>
2020-07-20 19:51:33 -0700

    Add rendering to the Palette

--- open-api-editor/src/components/ThePalette.vue
+++ open-api-editor/src/components/ThePalette.vue
@@ -4,16 +4,40 @@
     <div class="palette__servers__section">
       <h1 class="text-secondary">Servers</h1>
       <div class="palette__servers__editor-cards">
-        <ServerEditorCard />
+        <ServerEditorCard
+          v-for="(server, index) in servers"
+          :key="'server-editor-card' + index"
+          :index="index"
+        />
       </div>
-      <button>Add server</button>
+      <button @click="createServer()">Add server</button>
     </div>
     <div class="palette__paths__section">
       <h1 class="text-secondary">Paths</h1>
       <div class="palette__paths__editor-cards">
-        <PathEditorCard v-for="path in paths" :key="path.id" :id="path.id" />
+        <PathEditorCard
+          v-for="(path, index) in paths"
+          :key="'path-editor-card-' + index"
+          :index="index"
+        />
       </div>
       <button @click="createPath()">Add path</button>
+    </div>
+    <div class="palette__components__section">
+      <h1 class="text-secondary">Components</h1>
+      <p>
+        Components are reusable objects which can be used throughout your API
+        definition
+      </p>
+      <button>Add schema</button>
+      <button>Add response</button>
+      <button>Add parameter</button>
+      <button>Add example</button>
+      <button>Add request body</button>
+      <button>Add header</button>
+      <button>Add security scheme</button>
+      <button>Add link</button>
+      <button>Add callback</button>
     </div>
   </div>
 </template>
@@ -32,6 +56,9 @@ export default Vue.extend({
   computed: { ...mapState(["info", "servers", "paths"]) },
 
   methods: {
+    createServer() {
+      this.$store.commit("CREATE_SERVER");
+    },
     createPath() {
       this.$store.commit("CREATE_PATH");
     },
