f03f9bd210301326e1fc5232746cd0353cb3b2e8
theckert <thomas.james.eckert@gmail.com>
2020-09-02 20:22:46 -0700

    Add TheInfoCard

---
+++ open-api-editor/src/components/cards/TheInfoCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <div class=""></div>
+    <TextInput
+      label="Title"
+      placeholder="The title of the API"
+      :value="info.title"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { mapState } from "vuex";
+import TextInput from "@/components/inputs/TextInput.vue";
+
+export default Vue.extend({
+  name: "TheInfoCard",
+
+  components: {
+    TextInput
+  },
+
+  computed: {
+    ...mapState(["info"])
+  },
+
+  methods: {
+    updateTitle(e: any) {
+      this.$store.commit("UPDATE_TITLE", e.target.value);
+    }
+  }
+});
+</script>
