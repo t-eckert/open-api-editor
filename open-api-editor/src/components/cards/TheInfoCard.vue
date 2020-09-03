cd4b1f97df8a74edb59eef44cc1ae8893d448e28
theckert <thomas.james.eckert@gmail.com>
2020-09-03 08:43:14 -0700

    Fill out InfoCard

--- open-api-editor/src/components/cards/TheInfoCard.vue
+++ open-api-editor/src/components/cards/TheInfoCard.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="card">
-    <div class=""></div>
-    <TextInput
-      label="Title"
-      placeholder="The title of the API"
-      :value="info.title"
-    />
+    <div class="card__header">
+      <h2 class="text-secondary">Information</h2>
+    </div>
+    <div class="card__body grid col-2 gap">
+      <div>
+        <TextInput
+          label="Title"
+          placeholder="The title of the API"
+          :value="info.title"
+        />
+        <TheContactSection />
+      </div>
+      <div>
+        <TextInput label="API Version" placeholder="1.0.0" value="" />
+        <TextArea label="Description" />
+        <TheLicenseSection />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { mapState } from "vuex";
+import TheContactSection from "@/components/sections/TheContactSection.vue";
+import TheLicenseSection from "@/components/sections/TheLicenseSection.vue";
+import TextArea from "@/components/inputs/TextArea.vue";
 import TextInput from "@/components/inputs/TextInput.vue";
 
 export default Vue.extend({
   name: "TheInfoCard",
 
   components: {
+    TheContactSection,
+    TheLicenseSection,
+    TextArea,
     TextInput
   },
 
