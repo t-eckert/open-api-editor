e26a0231c744c047171af469b337e3af4807edd3
theckert <thomas.james.eckert@gmail.com>
2020-09-03 08:43:55 -0700

    Add contact and license sections

---
+++ open-api-editor/src/components/sections/TheLicenseSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="section">
+    <div class="section__body">
+      <h2 class="text-secondary">License</h2>
+      <TextInput label="Name" placeholder="Bill Posters" value="" />
+      <TextInput
+        label="Website"
+        placeholder="https://billposters.com"
+        value=""
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TextInput from "@/components/inputs/TextInput.vue";
+import { component } from "vue/types/umd";
+
+export default Vue.extend({
+  name: "TheLicenseSection",
+
+  components: {
+    TextInput
+  }
+});
+</script>
