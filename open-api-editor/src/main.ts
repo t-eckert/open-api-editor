228a4fe594263efbc04a3f97aa42da7f47af2024
theckert <thomas.james.eckert@gmail.com>
2020-09-03 22:50:14 -0700

    Remove unicons

--- open-api-editor/src/main.ts
+++ open-api-editor/src/main.ts
@@ -4,52 +4,6 @@ import "./registerServiceWorker";
 import store from "./store";
 import "./assets/normalize.css";
 import "./assets/styles.css";
-import Unicon from "vue-unicons";
-import {
-  uniCopy,
-  uniClipboard,
-  uniFileImport,
-  uniFileExport,
-  uniUsdSquare,
-  uniAngleDown,
-  uniAngleLeft,
-  uniAngleLeftB,
-  uniAngleRight,
-  uniAngleRightB,
-  uniAngleUp,
-  uniCheckCircle,
-  uniInfoCircle,
-  uniLinkAlt,
-  uniMultiply,
-  uniMinus,
-  uniPlus,
-  uniTimes
-  // @ts-ignore
-} from "vue-unicons/src/icons";
-
-// Add the Unicons to the app.
-// Unicons: https://antonreshetov.github.io/vue-unicons/
-Unicon.add([
-  uniCopy,
-  uniClipboard,
-  uniFileImport,
-  uniFileExport,
-  uniUsdSquare,
-  uniAngleDown,
-  uniAngleLeft,
-  uniAngleLeftB,
-  uniAngleRight,
-  uniAngleRightB,
-  uniAngleUp,
-  uniCheckCircle,
-  uniInfoCircle,
-  uniLinkAlt,
-  uniMultiply,
-  uniMinus,
-  uniPlus,
-  uniTimes
-]);
-Vue.use(Unicon);
 
 Vue.config.productionTip = false;
 
