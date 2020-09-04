67cd035a6e7ac155e0a65a02dd287c68239230b2
theckert <thomas.james.eckert@gmail.com>
2020-09-03 23:34:44 -0700

    Update navbar styles

--- open-api-editor/src/components/TheNavBar.vue
+++ open-api-editor/src/components/TheNavBar.vue
@@ -1,14 +1,16 @@
 <template>
   <nav>
-    <div>
-      <div><a class="nav-link" href="">Info</a></div>
-      <div><a class="nav-link" href="">Servers</a></div>
-      <div><a class="nav-link" href="">Paths</a></div>
-      <div><a class="nav-link" href="">Security</a></div>
-      <div><a class="nav-link" href="">Tags</a></div>
-    </div>
-    <div>
-      <div class="nav-link">Components</div>
+    <div class="nav-container">
+      <div class="nav-sections">
+        <div><a class="nav-link" href="">Info</a></div>
+        <div><a class="nav-link" href="">Servers</a></div>
+        <div><a class="nav-link" href="">Paths</a></div>
+        <div><a class="nav-link" href="">Security</a></div>
+        <div><a class="nav-link" href="">Tags</a></div>
+      </div>
+      <div class="nav-sections">
+        <div class="nav-link">Components</div>
+      </div>
     </div>
   </nav>
 </template>
