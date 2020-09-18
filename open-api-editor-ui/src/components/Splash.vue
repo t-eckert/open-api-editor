2cabb43685a5294b4631de9f909a9c8e47f09ec9
theckert <thomas.james.eckert@gmail.com>
2020-09-18 09:58:23 -0700

    Style splash

--- open-api-editor-ui/src/components/Splash.vue
+++ open-api-editor-ui/src/components/Splash.vue
@@ -1,24 +1,48 @@
 <template>
   <div>
-    <div class="w-full flex justify-between">
-      <h1 class="">Open API Editor</h1>
-      <button>Feedback about this page</button>
+    <div class="w-full flex justify-between pt-10 mb-16">
+      <h1 class="text-xl">Open API Editor</h1>
+      <button
+        @click="ui.feedbackModal = 'visible'"
+        class="px-2 py-1 rounded-lg block font-medium leading-5 text-gray-800 hover:bg-indigo-200 hover:text-gray-900"
+      >
+        Feedback
+      </button>
     </div>
-    <p>
-      The Open API Editor helps you build and maintain your Open API documents
-      without needing to know the specification.
-    </p>
-    <div>
-      <button @click="startNewDocument">Start new document</button>
-      <button @click="pasteDocument">Paste document</button>
+    <div class="ml-16 mb-16">
+      <p class="mb-6 text-3xl">
+        The <em>Open API Editor</em> helps you<br />build and maintain<br />your
+        Open API documents<br />without needing to know the specification.
+      </p>
+      <div class="flex">
+        <button
+          @click="startNewDocument"
+          class="mr-4 px-3 py-2 rounded-lg block font-medium leading-5 text-gray-900 bg-indigo-200 hover:bg-indigo-300"
+        >
+          Start new document
+        </button>
+        <button
+          @click="pasteDocument"
+          class="block px-3 py-2 font-medium leading-5 text-gray-900"
+        >
+          Paste document
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { ui } from "@/state";
 
 export default defineComponent({
   name: "Splash",
+
+  data() {
+    return {
+      ui,
+    };
+  },
 });
 </script>
