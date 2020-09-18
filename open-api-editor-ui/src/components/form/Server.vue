82c1ba55b59a49516f70ae646225f874ad640489
theckert <thomas.james.eckert@gmail.com>
2020-09-18 09:57:57 -0700

    Add Server and Servers

---
+++ open-api-editor-ui/src/components/form/Server.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-white overflow-hidden shadow rounded-lg">
+    <div
+      class="block px-4 pt-2 pb-1 text-sm font-medium leading-5 text-gray-700 bg-gray-100 border-b border-gray-200"
+    >
+      <h2>Server</h2>
+    </div>
+    <div class="p-4 sm:p-6 grid grid-cols-2 gap-4"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TextInput from "@/components/inputs/TextInput.vue";
+import TextArea from "@/components/inputs/TextArea.vue";
+import {} from "@/state";
+
+export default defineComponent({
+  name: "Information",
+
+  components: {
+    // TextInput,
+    // TextArea
+  },
+
+  data() {
+    return {};
+  }
+});
+</script>
---
+++ open-api-editor-ui/src/components/slots/Servers.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <div class="mb-4 flex w-full justify-between">
+      <h2 class="text-xl">Servers</h2>
+      <button
+        class="px-3 py-2 rounded-lg block font-medium leading-5 text-gray-900 bg-indigo-200 hover:bg-indigo-300"
+      >
+        Add server
+      </button>
+    </div>
+    <slot />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Servers"
+});
+</script>
