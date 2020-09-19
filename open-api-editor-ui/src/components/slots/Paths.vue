93ad5e5dabc71d847052ec1d99bb1fe5af127e7c
theckert <thomas.james.eckert@gmail.com>
2020-09-19 00:07:56 -0700

    Add Paths and Securities slots

---
+++ open-api-editor-ui/src/components/slots/Paths.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <div class="z-10 mb-4 pt-4 sticky top-0 bg-white bg-opacity-75">
+      <div class="container mx-auto flex justify-between">
+        <h2 class="text-xl">Paths</h2>
+        <button
+          @click="addPath"
+          class="px-3 py-2 rounded-lg block font-medium leading-5 text-gray-900 bg-indigo-200 hover:bg-indigo-300"
+        >
+          Add path
+        </button>
+      </div>
+    </div>
+    <div class="container mx-auto grid gap-4 grid-cols-2">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Paths",
+
+  methods: {
+    addPath() {
+      console.log("Not implemented");
+    }
+  }
+});
+</script>
