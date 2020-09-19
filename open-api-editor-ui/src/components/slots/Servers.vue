c0d0cf4172a118cc0c5d5893cc149510a753aab4
theckert <thomas.james.eckert@gmail.com>
2020-09-19 00:08:17 -0700

    Make Servers slot sticky

--- open-api-editor-ui/src/components/slots/Servers.vue
+++ open-api-editor-ui/src/components/slots/Servers.vue
@@ -1,15 +1,17 @@
 <template>
   <section>
-    <div class="mb-4 flex w-full justify-between">
-      <h2 class="text-xl">Servers</h2>
-      <button
-        @click="addServer"
-        class="px-3 py-2 rounded-lg block font-medium leading-5 text-gray-900 bg-indigo-200 hover:bg-indigo-300"
-      >
-        Add server
-      </button>
+    <div class="z-10 mb-4 pt-4 sticky top-0 bg-white bg-opacity-75">
+      <div class="container mx-auto flex justify-between">
+        <h2 class="text-xl">Servers</h2>
+        <button
+          @click="addServer"
+          class="px-3 py-2 rounded-lg block font-medium leading-5 text-gray-900 bg-indigo-200 hover:bg-indigo-300"
+        >
+          Add server
+        </button>
+      </div>
     </div>
-    <div class="grid gap-4 grid-cols-3">
+    <div class="container mx-auto grid gap-4 grid-cols-3">
       <slot />
     </div>
   </section>
