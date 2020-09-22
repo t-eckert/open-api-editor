c2f4ea64c3a13fb08f61aa57d2d7145649db14dc
theckert <thomas.james.eckert@gmail.com>
2020-09-21 23:39:48 -0700

    Added "REMOVE" on hover to Server

--- open-api-editor-ui/src/components/form/Server.vue
+++ open-api-editor-ui/src/components/form/Server.vue
@@ -4,24 +4,36 @@
       class="flex justify-between items-center block px-4 pt-2 pb-1 text-sm font-medium leading-5 text-gray-700 bg-gray-100 border-b border-gray-200"
     >
       <h2>{{ server.url || "Server" }}</h2>
-      <div @click="removeServer" class="pointer hover:bg-indigo-100">
-        <svg
-          class="h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      <div class="flex items-center cursor-pointer">
+        <span
+          class="text-xs uppercase mr-1 text-gray-600"
+          :class="isRemoveVisible ? 'visible' : 'invisible'"
+          >Remove {{ server.url || "server" }}</span
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <span
+          @click="removeServer"
+          @mouseover="isRemoveVisible = true"
+          @mouseleave="isRemoveVisible = false"
+          class="pointer hover:bg-indigo-100"
+        >
+          <svg
+            class="h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </span>
       </div>
     </div>
-    <div class="p-4 sm:p-6">
+    <div class="p-2 sm:p-4">
       <text-input
         v-model:value="server.url"
         label="Url"
@@ -32,6 +44,7 @@
         v-model:value="server.description"
         label="Description"
         placeholder="A description of your server"
+        isOptional
       />
     </div>
   </div>
@@ -61,7 +74,8 @@ export default defineComponent({
 
   data() {
     return {
-      server: servers[this.index]
+      server: servers[this.index],
+      isRemoveVisible: false
     };
   },
 
