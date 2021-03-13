f91b685163f4d65ac16655c14cb21361f124b722
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-12 19:31:04 -0800

    Change App from JS to TS

---
+++ site/src/App.tsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <header className=""></header>
+      <main className=""></main>
+      <footer className=""></footer>
+    </div>
+  );
+}
+
+export default App;
