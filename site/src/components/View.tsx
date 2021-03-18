7bcbc5b6d6cde431548b1b7af55ca33b1c8dd00d
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-17 22:33:42 -0700

    View Component renders from UI store

--- site/src/components/View.tsx
+++ site/src/components/View.tsx
@@ -1,11 +1,12 @@
-import DocumentsView from "./views/DocumentsView";
+import { observer } from "mobx-react-lite";
+import { useContext } from "react";
 
-const View = () => {
-  return (
-    <div>
-      <DocumentsView />
-    </div>
-  );
-};
+import { UiStoreContext } from "../stores/ui";
+
+const View = observer(() => {
+  const ui = useContext(UiStoreContext);
+
+  return <div>{ui.viewComponent()}</div>;
+});
 
 export default View;
