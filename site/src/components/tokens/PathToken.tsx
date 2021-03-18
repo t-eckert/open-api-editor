95e7dcf3125d233c23f3d6f561dcada271db7a85
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 09:39:02 -0700

    Change TitleToken to PathToken

---
+++ site/src/components/tokens/PathToken.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const PathToken = () => {
+  return <Title />;
+};
+
+const Title = () => (
+  <div className="flex flex-row justify-start items-center gap-2 z-10 py-1.5 font-medium text-sm">
+    <img className="h-5 w-5" src="/logo.png" alt="Open API Editor logo" />
+    <h1>
+      <Link to="/">Open API Editor</Link>
+    </h1>
+  </div>
+);
+
+export default PathToken;
