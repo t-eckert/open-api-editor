f70108ba0b0cfd4aab0742a9cfad2b2a99f0217f
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 03:49:22 -0700

    Structure interface module

--- site/src/interfaces/user.ts
+++ site/src/interfaces/user.ts
@@ -2,5 +2,6 @@ export interface User {
   id: string
   email: string
   name: string
-  isPro: string
+  profileIcon?: string // TODO 2020-03-17 should this be a URL type? It's a URL...
+  isPro: boolean
 }
