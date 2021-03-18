68b168368a8c0e1a63d120629e77331189229dde
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 03:47:24 -0700

    UiStore for setting current view

--- site/src/stores/ui.ts
+++ site/src/stores/ui.ts
@@ -1,22 +1,13 @@
-import { observable } from "mobx";
 import { createContext } from "react";
 
-import DocumentsView from "../components/views/DocumentsView"
-import EditorView from "../components/views/EditorView"
-import SettingsView from "../components/views/SettingsView"
 import View from "../types/View";
 
-const viewComponents = new Map([
-  ["documents", DocumentsView],
-  ["editor", EditorView],
-  ["settings", SettingsView]
-])
-
 class UiStore {
-  @observable view: View = "documents"
+  view: View = "documents"
 
-  get viewComponent(): any {
-    return viewComponents.get(this.view)
+  updateView(view: View) {
+    console.log("View changed to " + view)
+    this.view = view
   }
 }
 
