cc5470b7f30b4c25e175d01e0d234e7ac399f999
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 03:46:54 -0700

    Implement a basic UserStore

--- site/src/stores/user.ts
+++ site/src/stores/user.ts
@@ -1,3 +1,18 @@
-export class User {
+import { createContext } from "react";
+import { User } from "../interfaces/user";
 
+class UserStore {
+  user: User = {
+    id: "0",
+    email: "jc@email.com",
+    name: "Jane Cooper",
+    profileIcon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60",
+    isPro: false
+  }
+
+  fetchUser() {
+
+  }
 }
+
+export const UserStoreContext = createContext(new UserStore())
