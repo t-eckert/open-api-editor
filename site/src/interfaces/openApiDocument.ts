77a1e1c08ad0333d25c6891110cd119878611a90
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 16:07:09 -0700

    Begin to flesh out OpenApiDocument interface

--- site/src/interfaces/openApiDocument.ts
+++ site/src/interfaces/openApiDocument.ts
@@ -1,3 +1,21 @@
 export interface OpenApiDocument {
+  id: string
 
+  title: string
+  version: string
+  description: string
+  termsOfService?: string
+
+  contactInformation?: {
+    name?: string
+    email?: string
+    website?: string
+  }
+
+  license?: {
+    name?: string
+    url?: string
+  }
+
+  servers: {}[]
 }
