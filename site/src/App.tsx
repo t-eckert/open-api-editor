2d415697b077f5e20c8e001818fe1c4ca134f2ea
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 16:06:43 -0700

    Display the Changelog in the UI

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom"
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import View from "./components/View";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import View from "./View"
 
 const App = () => {
   return (
     <Router>
+      <div id="page-top" />
       <div className="bg-yellow-50 min-h-screen overflow-x-hidden">
         <Header />
         <View />
       </div>
       <Footer />
+      <div id="page-bottom" />
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- site/src/config.ts
+++ site/src/config.ts
@@ -1,1 +1,3 @@
 export const API = "https://api.openapieditor.com"
+
+export const CHANGELOG_URL = "https://raw.githubusercontent.com/t-eckert/open-api-editor/main/CHANGELOG.md"
