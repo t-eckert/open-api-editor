535e3b8137e9a0d7c78468226191b63b96a1327a
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 16:05:39 -0700

    Remove semicolons from MenuToken

--- site/src/components/tokens/MenuToken.tsx
+++ site/src/components/tokens/MenuToken.tsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import Menu from "../Menu";
-import MenuIcon from "../icons/MenuIcon";
-import FeedbackIcon from "../icons/FeedbackIcon";
-import SettingsIcon from "../icons/SettingsIcon";
-import LogoutIcon from "../icons/LogoutIcon";
+import Menu from "../Menu"
+import MenuIcon from "../icons/MenuIcon"
+import FeedbackIcon from "../icons/FeedbackIcon"
+import SettingsIcon from "../icons/SettingsIcon"
+import LogoutIcon from "../icons/LogoutIcon"
 
 const menuToggleClasses = (showMenu: boolean) =>
   "border-l pl-1 pr-2 py-1.5 rounded-r-xl cursor-pointer hover:bg-gray-100 " +
-  (showMenu ? "bg-gray-100" : "bg-white");
+  (showMenu ? "bg-gray-100" : "bg-white")
 
 type MenuItem = {
-  text: string;
-  icon: any;
-  link: string;
-  classes?: string;
-};
+  text: string
+  icon: any
+  link: string
+  classes?: string
+}
 
 const itemClasses =
-  "flex flex-row items-center gap-3 w-full text-left px-4 py-1.5 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 ";
+  "flex flex-row items-center gap-3 w-full text-left px-4 py-1.5 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 "
 
 const menuItems = [
   {
@@ -36,17 +36,17 @@ const menuItems = [
     {menuItem.icon}
     {menuItem.text}
   </button>
-));
+))
 
 menuItems.push(
   <button className={itemClasses}>
     <LogoutIcon classes="h-4 w-4 text-gray-700" />
     Logout
   </button>
-);
+)
 
 const MenuToken = () => {
-  let [showMenu, setShowMenu] = useState(false);
+  let [showMenu, setShowMenu] = useState(false)
 
   return (
     <div>
@@ -66,7 +66,7 @@ const MenuToken = () => {
         <div></div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default MenuToken;
+export default MenuToken
