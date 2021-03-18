b018b1ce558521bd6f509204828f5db117ab0585
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 03:44:14 -0700

    Split out user and menu token. Increase token height.

--- site/src/components/commandPalette/CommandPalette.tsx
+++ site/src/components/commandPalette/CommandPalette.tsx
@@ -2,7 +2,7 @@ const CommandPalette = (props: any) => {
   return (
     <div className="shadow rounded-xl w-full mx-auto max-w-lg">
       <input
-        className="w-full px-2 py-1 rounded-xl text-sm"
+        className="w-full px-2 py-1.5 rounded-xl text-sm"
         type="text"
         name="command-palette"
         id="command-palette"
