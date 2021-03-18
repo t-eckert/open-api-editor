d5b1221dfd3307dddb41b53d2d6cda0822c72af3
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 11:36:51 -0700

    Connect the control palette to a Mobx store

--- site/src/components/commandPalette/CommandPalette.tsx
+++ site/src/components/commandPalette/CommandPalette.tsx
@@ -1,15 +1,26 @@
-const CommandPalette = (props: any) => {
-  return (
-    <div className="shadow rounded-xl w-full mx-auto max-w-lg">
-      <input
-        className="w-full px-2 py-1.5 rounded-xl text-sm"
-        type="text"
-        name="command-palette"
-        id="command-palette"
-        placeholder="Command palette"
-      />
-    </div>
-  );
-};
+import { observer } from "mobx-react-lite"
+import { useContext } from "react"
 
-export default CommandPalette;
+import { CommandPaletteStoreContext } from "../../stores/commandPalette"
+
+const CommandPalette = observer(
+  (): JSX.Element => {
+    const commandPaletteStore = useContext(CommandPaletteStoreContext)
+
+    return commandPaletteStore.isVisible ? (
+      <div className="shadow rounded-xl w-full mx-auto max-w-lg">
+        <input
+          className="w-full px-2 py-1.5 rounded-xl text-sm"
+          type="text"
+          name="command-palette"
+          id="command-palette"
+          placeholder="Command palette"
+        />
+      </div>
+    ) : (
+      <div></div>
+    )
+  }
+)
+
+export default CommandPalette
