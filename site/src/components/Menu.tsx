79806f29c295081631dbf7ea3ee483cbb190c6e4
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 09:42:07 -0700

    Pull menu item styling to parent

--- site/src/components/Menu.tsx
+++ site/src/components/Menu.tsx
@@ -1,13 +1,6 @@
-export type MenuItem = {
-  onClick: any;
-  classes?: string;
-  icon?: any;
-  text: string;
-};
-
 type Props = {
   wrappingClasses: string;
-  menuItems: MenuItem[];
+  menuItems: any[];
 };
 
 const Menu = (props: Props) => {
@@ -16,24 +9,10 @@ const Menu = (props: Props) => {
     props.wrappingClasses,
   ].join(" ");
 
-  const itemClasses =
-    "flex flex-row items-center gap-3 w-full text-left px-4 py-1.5 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 ";
-
-  const items = props.menuItems.map((menuItem: MenuItem) => (
-    <button
-      onClick={menuItem.onClick}
-      className={itemClasses + menuItem.classes}
-      role="menuitem"
-    >
-      {menuItem.icon}
-      {menuItem.text}
-    </button>
-  ));
-
   return (
     <div className={menuClasses}>
       <div className="py-1" role="none">
-        {items}
+        {props.menuItems}
       </div>
     </div>
   );
