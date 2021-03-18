327325649268e5aea49f5ab40b20bd0d09caa2b0
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-18 11:37:39 -0700

    Move Command Palette logic down into the component itself

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Router>
       <div className="bg-yellow-50 min-h-screen overflow-x-hidden">
-        <Header documentName="" showCommandPalette={true} user="" />
+        <Header />
         <View />
       </div>
       <Footer />
