c40a6ba29232de1ff63d17cc7e2414f04cda510e
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-21 14:29:20 -0700

    Remove old models

--- api/models/user.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from mongoengine import Document, ListField, ReferenceField, StringField
-
-
-@dataclass
-class User:
-    email = StringField(require=True)
-    name = StringField(required=True)
