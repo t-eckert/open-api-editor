e9f74f968a19ff0ee7a8b200b01c447a65af4a82
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-25 20:14:17 -0700

    Add GITHUB_APP_CLIENT_ID for prod

--- site/src/config.ts
+++ site/src/config.ts
@@ -1,15 +1,18 @@
 export const CHANGELOG_URL = "https://raw.githubusercontent.com/t-eckert/open-api-editor/main/CHANGELOG.md"
-export const GITHUB_APP_CLIENT_ID = "d2e20ee498c0e3d317fe"
 
 const environment = process.env.NODE_ENV
 
 let API: string
+let GITHUB_APP_CLIENT_ID: string
 if ("production" === environment) {
   API = "https://api.openapieditor.com/api"
+  GITHUB_APP_CLIENT_ID = "c107f43307dff88c2d16"
 } else if ("development" === environment) {
   API = "http://localhost:7071/api"
+  GITHUB_APP_CLIENT_ID = "d2e20ee498c0e3d317fe"
 } else {
   API = "http://localhost:7071/api"
+  GITHUB_APP_CLIENT_ID = "d2e20ee498c0e3d317fe"
 }
 
-export { API }
+export { API, GITHUB_APP_CLIENT_ID }
