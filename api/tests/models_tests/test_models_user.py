7140e59d0129b97f27376ee0baa3bd487b19dbf1
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-27 23:11:41 -0700

    Fix User model tests

--- api/tests/models_tests/test_models_user.py
+++ api/tests/models_tests/test_models_user.py
@@ -1,5 +1,6 @@
 from lib.models import OpenApiDocument, User
 
+import json
 import pytest
 
 
@@ -7,15 +8,17 @@ def test_from_json():
     # Given
     email = "bill.posters@email.com"
     name = "Bill Posters"
-    profile_picture = "https://images.unsplash.com/photo-1611774119019-461b5dbd3ae8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80"
+    picture = "https://images.unsplash.com/photo-1611774119019-461b5dbd3ae8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80"
 
-    user_json = {
-        "email": email,
-        "name": name,
-        "profilePicture": profile_picture,
-    }
+    user_json = json.dumps(
+        {
+            "email": email,
+            "name": name,
+            "picture": picture,
+        }
+    )
 
-    expected = User(email=email, name=name, profile_picture=profile_picture)
+    expected = User(email=email, name=name, picture=picture)
 
     # When
     actual = User.from_json(user_json)
@@ -23,7 +26,7 @@ def test_from_json():
     # Then
     assert expected.email == actual.email
     assert expected.name == actual.name
-    assert expected.profile_picture == actual.profile_picture
+    assert expected.picture == actual.picture
 
 
 @pytest.mark.parametrize(
@@ -33,8 +36,7 @@ def test_from_json():
             User(
                 name="Bill Posters",
                 email="bill.posters@email.com",
-                is_pro=False,
-                hashed_password="",
+                isPro=False,
                 documents=[OpenApiDocument(), OpenApiDocument()],
             ),
             False,
@@ -43,8 +45,7 @@ def test_from_json():
             User(
                 name="Bill Posters",
                 email="bill.posters@email.com",
-                is_pro=True,
-                hashed_password="",
+                isPro=True,
                 documents=[OpenApiDocument(), OpenApiDocument()],
             ),
             True,
@@ -53,8 +54,7 @@ def test_from_json():
             User(
                 name="Bill Posters",
                 email="bill.posters@email.com",
-                is_pro=False,
-                hashed_password="",
+                isPro=False,
                 documents=[OpenApiDocument()],
             ),
             True,
@@ -63,8 +63,7 @@ def test_from_json():
             User(
                 name="Bill Posters",
                 email="bill.posters@email.com",
-                is_pro=False,
-                hashed_password="",
+                isPro=False,
                 documents=[],
             ),
             True,
