c86f031fd18f0b13414a1163e20c351038557702
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-29 23:15:08 -0700

    Add a sentry module

---
+++ api/lib/sentry.py
@@ -0,0 +1,10 @@
+from lib.config import SENTRY_URL, SENTRY_CONFIG
+from sentry_sdk.integrations.serverless import serverless_function
+
+import sentry_sdk
+
+
+def connect_to_sentry():
+    """Creates a connection to Sentry for logging"""
+
+    sentry_sdk.init(SENTRY_URL, traces_sample_rate=1.0, **SENTRY_CONFIG)
