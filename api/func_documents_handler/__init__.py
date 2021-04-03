d0e335168c95f4407047c8aba954b2670c57ae0c
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-02 17:12:26 -0700

    Auth the user on document interactions

--- api/func_documents_handler/__init__.py
+++ api/func_documents_handler/__init__.py
@@ -1,6 +1,9 @@
 from azure.functions import HttpRequest, HttpResponse
 from func_documents_handler.documents_router import documents_router
+from lib.auth import jwt
+from lib.models import User
 from lib.sentry import connect_to_sentry, serverless_function
+from typing import Optional
 
 import logging
 
@@ -12,6 +15,18 @@ connect_to_sentry()
 def handle_documents_request(request: HttpRequest) -> HttpResponse:
     logging.info(f"/documents recieved a {request.method} request")
 
-    response: HttpResponse = documents_router.route(request.method, request)
+    token: Optional[str] = request.headers["token"]
+
+    if not token:
+        return HttpResponse("Invalid auth token", status_code=401)
+
+    try:
+        user_info: dict = jwt.decode(token)
+    except:
+        return HttpResponse("Invalid auth token", status_code=401)
+
+    user: User = User.from_json(user_info)
+
+    response: HttpResponse = documents_router.route(request.method, user, request)
 
     return response or HttpResponse("Could not process request")
