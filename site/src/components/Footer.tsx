e37c97fe978f3811eb7f1b61386de44a8d5718e1
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-02 17:06:47 -0700

    Change Footer version to 0.0.1

--- site/src/components/Footer.tsx
+++ site/src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <section className="mx-auto pt-8 container grid grid-cols-3">
         <div className="font-medium text-white">
           <p>
-            Open API Editor version <code>0.1.0</code>
+            Open API Editor version <code>0.0.1</code>
           </p>
           <AnchorLink href="#page-top">
             <Link to="/changelog">Changelog</Link>
