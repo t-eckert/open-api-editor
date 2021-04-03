0a73ef84fbd0e21f90034a81ef732575acd31fea
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-02 17:10:26 -0700

    Add a single line separation in UserToken component

--- site/src/components/tokens/UserToken.tsx
+++ site/src/components/tokens/UserToken.tsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react-lite"
 import { useContext } from "react"
 
 import { UserStoreContext } from "../../stores/user"
+
 import LoginWithGitHub from "../buttons/LoginWithGitHubButton"
 
 const UserToken = observer(() => {
