51d67534d561981a842f4cdd0b7c90cbab2c5d6a
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-02 17:10:07 -0700

    Change formatting in Button component

--- site/src/components/buttons/Button.tsx
+++ site/src/components/buttons/Button.tsx
@@ -1,29 +1,29 @@
-type Role = "primary" | "secondary" | "tertiary" | "link";
+type Role = "primary" | "secondary" | "tertiary" | "link"
 
 type Props = {
-  role: Role;
-  action: any;
-  children: any;
-};
+  role: Role
+  action: any
+  children: any
+}
 
 const classRoles = new Map([
   ["primary", ""],
   [
     "secondary",
-    "font-medium text-yellow-500 shadow bg-white px-3 py-1 rounded-full hover:text-white hover:bg-yellow-500 transition",
+    "font-medium text-yellow-500 shadow bg-white px-3 py-1 rounded-full hover:text-white hover:bg-yellow-500 transition focus:outline-none focus:ring",
   ],
   ["tertiary", "font-medium text-yellow-500 hover:text-yellow-600 transition"],
   ["link", "font-medium text-gray-600 hover:text-gray-900 transition"],
-]);
+])
 
 const Button = (props: Props) => {
-  const classes = classRoles.get(props.role) ?? "";
+  const classes = classRoles.get(props.role) ?? ""
 
   return (
     <button className={classes} onClick={props.action}>
       {props.children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
