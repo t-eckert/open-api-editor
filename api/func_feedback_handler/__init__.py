0312785c8b9d60db5451dbbdf3e0a8a56be24202
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-03 19:11:17 -0700

    Add Email class to send feedback

--- api/func_feedback_handler/__init__.py
+++ api/func_feedback_handler/__init__.py
@@ -1,4 +1,7 @@
+from lib.models.feedback import Feedback
 from azure.functions import HttpRequest, HttpResponse
+from lib.config import DEV_EMAIL
+from lib.email import Email
 from lib.sentry import connect_to_sentry, serverless_function
 
 import logging
@@ -18,5 +21,18 @@ def handle_feedback_request(request: HttpRequest) -> HttpResponse:
     """
 
     logging.info("api/feedback called")
+
+    try:
+        body: dict = request.get_json()
+        feedbackBody: str = body.get("feedbackBody")
+        email: str = body.get("email")
+    except ValueError:
+        return HttpResponse("Body not formatted as JSON", status_code=400)
+
+    feedback = Feedback(feedbackBody, email)
+
+    subject = "Feedback from " + feedback.email or "anonymous"
+    email_message = Email(feedback.email or DEV_EMAIL, [DEV_EMAIL], subject, feedback.feedbackBody)
+    email_message.send()
 
     return HttpResponse("Ok")
