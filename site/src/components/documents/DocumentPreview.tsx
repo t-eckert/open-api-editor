052ee85f7cab770a5aae8df6bde3b48219444b9d
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-03 19:16:43 -0700

    Add focus styles to DocumentPreview

--- site/src/components/documents/DocumentPreview.tsx
+++ site/src/components/documents/DocumentPreview.tsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 type Props = {
   document: {
-    title: string;
-    version: string;
-  };
-};
+    title: string
+    version: string
+  }
+}
 
 const classes = [
   "z-10 p-3 flex bg-white shadow justify-between transition", // Both widths
   "w-full h-15 flex-row items-center border-b border-yellow first:rounded-t-md last:border-none", // Small width
   "md:w-44 md:h-60 md:flex-col md:items-start md:border-none md:shadow md:rounded-md md:hover:shadow-xl", // Large width
-].join(" ");
+].join(" ")
 
 const DocumentPreview = (props: Props) => {
   return (
@@ -21,13 +21,13 @@ const DocumentPreview = (props: Props) => {
         <h3 className="font-medium text-gray-600">{props.document.version}</h3>
       </div>
       <Link
-        className="font-medium text-yellow-500 hover:text-yellow-600 transition"
+        className="font-medium rounded text-yellow-500 hover:text-yellow-600 focus focus:ring-offset-2 transition"
         to="/editor/2"
       >
         Edit
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default DocumentPreview;
+export default DocumentPreview
