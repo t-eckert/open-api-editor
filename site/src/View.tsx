228341ba773d283cbd65f754b9587d2094fe95aa
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-03 17:56:16 -0700

    Add the feedback view to Router

--- site/src/View.tsx
+++ site/src/View.tsx
@@ -4,6 +4,7 @@ import DocumentsView from "./views/DocumentsView"
 import EditorView from "./views/EditorView"
 import SettingsView from "./views/SettingsView"
 import ChangelogView from "./views/ChangelogView"
+import FeedbackView from "./views/FeedbackView"
 import LoginView from "./views/LoginView"
 
 const View = () => {
@@ -13,6 +14,7 @@ const View = () => {
       <Route path="/editor/:id" component={EditorView} />
       <Route path="/settings" component={SettingsView} />
       <Route path="/changelog" component={ChangelogView} />
+      <Route path="/feedback" component={FeedbackView} />
       <Route path="/login" component={LoginView} />
     </div>
   )
