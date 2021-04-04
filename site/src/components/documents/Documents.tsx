7c9fe8100a43c5504a0030002e1ef36a318379bb
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-03 19:16:26 -0700

    Remove Recently updated to Documents

--- site/src/components/documents/Documents.tsx
+++ site/src/components/documents/Documents.tsx
@@ -1,29 +1,26 @@
-import DocumentPreview from "./DocumentPreview";
-import DocumentCreate from "./DocumentCreate";
+import DocumentPreview from "./DocumentPreview"
+import DocumentCreate from "./DocumentCreate"
 
 type Props = {
-  documents: { title: string; version: string }[];
-};
+  documents: { title: string; version: string }[]
+}
 
 const Documents = (props: Props) => {
   const documentPreviews = props.documents.map((document) => (
     <DocumentPreview document={document} key={document.title} />
-  ));
+  ))
 
   return (
     <div className="pt-8 px-2 mx-auto container">
       <div className="w-full flex flex-row justify-between items-baseline">
         <h1 className="mb-4 font-medium text-xl">Documents</h1>
-        <div className="font-medium text-sm text-yellow-900">
-          Recently updated
-        </div>
       </div>
       <div className="flex flex-col md:flex-row flex-wrap md:gap-4">
         {documentPreviews}
         <DocumentCreate />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Documents;
+export default Documents
