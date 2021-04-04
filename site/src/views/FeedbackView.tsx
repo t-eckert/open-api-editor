9fceb01af73798691a631cbfddb2f2c31b62c5a3
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-03 19:13:34 -0700

    Add margin to top of feedback form

--- site/src/views/FeedbackView.tsx
+++ site/src/views/FeedbackView.tsx
@@ -3,7 +3,7 @@ import FeedbackForm from "../components/FeedbackForm"
 const FeedbackView = () => {
   return (
     <div className="w-full">
-      <section className="p-3 mt-8 bg-white rounded-xl shadow max-w-xl mx-auto">
+      <section className="p-3 mt-16 bg-white rounded-xl shadow max-w-xl mx-auto">
         <FeedbackForm />
       </section>
     </div>
