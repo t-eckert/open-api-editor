1b4e1cadf8b9125d1e3eb6eda6325bf01662eacb
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-04 05:16:41 -0700

    Change config to remove /api from API calls

--- site/src/config.ts
+++ site/src/config.ts
@@ -6,13 +6,13 @@ const environment = process.env.NODE_ENV
 let API: string
 let GITHUB_APP_CLIENT_ID: string
 if ("production" === environment) {
-  API = "https://api.openapieditor.com/api"
+  API = "https://api.openapieditor.com"
   GITHUB_APP_CLIENT_ID = "c107f43307dff88c2d16"
 } else if ("development" === environment) {
-  API = "http://localhost:7071/api"
+  API = "http://localhost:7071"
   GITHUB_APP_CLIENT_ID = "d2e20ee498c0e3d317fe"
 } else {
-  API = "http://localhost:7071/api"
+  API = "http://localhost:7071"
   GITHUB_APP_CLIENT_ID = "d2e20ee498c0e3d317fe"
 }
 
