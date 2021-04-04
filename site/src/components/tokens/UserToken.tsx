08bc7aeadf8968d71b4bee3b79e3abc0d7fc5c81
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-03 19:19:26 -0700

    use useUser hook

--- site/src/components/tokens/UserToken.tsx
+++ site/src/components/tokens/UserToken.tsx
@@ -1,26 +1,21 @@
 import { observer } from "mobx-react-lite"
-import { useContext } from "react"
-
-import { UserStoreContext } from "../../stores/user"
+import useUser from "../../hooks/useUser"
 
 import LoginWithGitHub from "../buttons/LoginWithGitHubButton"
 
 const UserToken = observer(() => {
-  const userStore = useContext(UserStoreContext)
-  const user = userStore.user
+  const user = useUser()
 
   return (
     <div className="flex flex-row items-center rounded-xl">
       {user ? (
         <div className="bg-white rounded-l-xl flex flex-row items-center">
-          {user.picture ? (
+          {user.picture && (
             <img
               className="w-8 h-8 bg-gray-300 rounded-l-xl flex-shrink-0 object-cover"
               src={user.picture}
               alt={`The profile for ${user.name}`}
             />
-          ) : (
-            <div></div>
           )}
           <div className="font-medium text-sm px-2 py-1.5">{user.name}</div>
         </div>
