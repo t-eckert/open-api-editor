a1ee27c0d3fc0a254bfadc772dfe15ece5640a83
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-08 04:43:56 -0700

    Add documentation to the database decorator

--- api/lib/database.py
+++ api/lib/database.py
@@ -3,8 +3,31 @@ from mongoengine import connect, disconnect
 from typing import Any, Callable
 
 
-def db(function: Callable) -> Callable:
+def database(function: Callable) -> Callable:
+    """Decorator providing a database connection for the lifetime of a function
+
+    Usage:
+
+    ```python
+    @database
+    def save_document(document: Document):
+        document.save()
+    ```
+
+    Args:
+        function (Callable):    function to be decorated with database access
+
+    Returns:
+        Callable:               function, decorated with database access
+    """
+
     def connect_to_database(*args, **kwargs) -> Any:
+        """Connects to the database prior to calling the curried function. Disposes connection after call completes.
+
+        Returns:
+            Any:                value returned by the decorated function
+        """
+
         connect(host=DATABASE_CONNECTION_STRING)
         return_value = function(*args, **kwargs)
         disconnect()
