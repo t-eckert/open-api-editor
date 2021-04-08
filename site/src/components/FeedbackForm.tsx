747a79da10d7c8527b309c158418ff827bac050e
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-08 00:30:54 -0700

    Send feedback values to backend

--- site/src/components/FeedbackForm.tsx
+++ site/src/components/FeedbackForm.tsx
@@ -1,25 +1,42 @@
+import { useInput } from "../hooks"
+import { submitFeedback } from "../actions"
 import Button from "./buttons/Button"
 import TextArea from "./inputs/TextArea"
 import TextInput from "./inputs/TextInput"
 
 const FeedbackForm = () => {
+  const {
+    value: feedbackBody,
+    bind: bindFeedbackBody,
+    reset: resetFeedbackBody,
+  } = useInput("")
+  const { value: email, bind: bindEmail, reset: resetEmail } = useInput("")
+
+  const onSubmit = (evt: any) => {
+    evt.preventDefault()
+    submitFeedback({ feedbackBody, email })
+    resetFeedbackBody()
+    resetEmail()
+  }
+
   return (
-    <form className="flex flex-col gap-3 items-end">
+    <form className="flex flex-col gap-3 items-end" onSubmit={onSubmit}>
       <TextArea
         className="h-40 flex flex-col w-full"
         label="Feedback"
         placeholder="Write a little feedback about your experience"
         id="feedback"
-        value=""
         isOptional={false}
+        {...bindFeedbackBody}
       />
       <TextInput
         className="w-full"
         label="Email"
         placeholder="bill.posters@email.com"
         id="email"
-        value=""
+        type="email"
         isOptional={true}
+        {...bindEmail}
       />
       <Button role="primary" action={() => {}}>
         Submit feedback
