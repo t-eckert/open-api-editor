b562060e3b9ca021bf3ea42da753a5df98ee83ca
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-10 05:27:12 -0700

    Clean up MenuToken with a useClickToggler hook

--- site/src/components/tokens/MenuToken.tsx
+++ site/src/components/tokens/MenuToken.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { useContext, useState } from "react"
+import { useContext, useRef } from "react"
 import { observer } from "mobx-react-lite"
 
 import { UserStoreContext } from "../../stores/user"
@@ -9,24 +9,18 @@ import LogoutButton from "../buttons/LogoutButton"
 import MenuIcon from "../icons/MenuIcon"
 import FeedbackIcon from "../icons/FeedbackIcon"
 import SettingsIcon from "../icons/SettingsIcon"
-
-const menuToggleClasses = (showMenu: boolean) =>
-  [
-    "border-l pl-1 pr-2 py-1.5 rounded-r-xl cursor-pointer hover:bg-gray-100",
-    "focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-60",
-    showMenu ? "bg-gray-100" : "bg-white",
-  ].join(" ")
+import { useClickToggler } from "../../hooks"
 
 type MenuItem = {
   text: string
   icon: any
   link: string
-  classes?: string
+  className?: string
 }
 
 const formatMenuItem = (menuItem: MenuItem) => (
   <Link
-    className="menuitem"
+    className="menu-item"
     role="menuitem"
     key={menuItem.link}
     to={menuItem.link}
@@ -36,20 +30,31 @@ const formatMenuItem = (menuItem: MenuItem) => (
   </Link>
 )
 
+/** `MenuToken` component
+ *
+ * Toggles a menu for handling universal options and routing to Settings and Feedback views
+ */
 const MenuToken = observer(() => {
+  const menuRef = useRef<HTMLDivElement>(null)
+  const { toggleMode: showMenu, setToggleMode: setShowMenu } = useClickToggler(
+    menuRef,
+    false
+  )
+
   const userStore = useContext(UserStoreContext)
   const user = userStore.user
 
-  const [showMenu, setShowMenu] = useState(false)
+  // Define the menu items based on if the user is logged in
 
   const menuItems = [
     formatMenuItem({
       text: "Feedback",
-      icon: FeedbackIcon({ classes: "h-4 w-4 text-gray-700" }),
+      icon: FeedbackIcon({ className: "h-4 w-4 text-gray-700" }),
       link: "/feedback",
     }),
   ]
 
+  // If a user is logged in, give them the ability to view settings or log out
   if (user) {
     menuItems.push(
       formatMenuItem({
@@ -58,22 +63,21 @@ const MenuToken = observer(() => {
         link: "/settings",
       })
     )
-    menuItems.push(<LogoutButton className="menuitem" />)
+    menuItems.push(<LogoutButton className="menu-item" />)
   }
 
   return (
-    <div>
+    <div ref={menuRef}>
       <button
         onClick={() => setShowMenu(!showMenu)}
-        className={menuToggleClasses(showMenu)}
+        className={"menu-toggle " + (showMenu ? "bg-gray-100" : "bg-white")}
       >
-        <MenuIcon classes="h-5 w-5" />
+        <MenuIcon className="h-5 w-5" />
       </button>
       {showMenu && (
-        <Menu
-          wrappingClasses="mt-2 origin-top-right absolute right-2"
-          menuItems={menuItems}
-        />
+        <Menu wrappingClasses="mt-2 origin-top-right absolute right-2">
+          {menuItems}
+        </Menu>
       )}
     </div>
   )
--- site/src/hooks/index.ts
+++ site/src/hooks/index.ts
@@ -1,6 +1,7 @@
 import useChangelog from "./useChangelog"
+import useClickToggler from "./useClickToggler"
 import useInput from "./useInput"
 import useScrollTo from "./useScrollTo"
 import useUserStore from "./useUserStore"
 
-export { useChangelog, useInput, useScrollTo, useUserStore }
+export { useChangelog, useClickToggler, useInput, useScrollTo, useUserStore }
