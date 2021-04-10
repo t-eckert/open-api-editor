a071002aa7d92a57ea58f21ec6dbdd069cb257d3
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-10 09:10:28 -0700

    Fix config to use CRACO

--- site/src/functions/generateGitHubLoginUrl.test.ts
+++ site/src/functions/generateGitHubLoginUrl.test.ts
@@ -5,10 +5,10 @@ it("generates a GitHub login URL with given state", () => {
   // Given
   const state = "a1b2c3d4"
 
-  const expected = new URL(`${GITHUB_OAUTH_URL}?client_id=${GITHUB_APP_CLIENT_ID}&state=${state}`)
+  const expected = `${GITHUB_OAUTH_URL}?client_id=${GITHUB_APP_CLIENT_ID}&state=${state}`
 
   // When
-  const actual = generateGitHubLoginUrl(state)
+  const actual = generateGitHubLoginUrl(state).toString()
 
   // Then
   expect(actual).toEqual(expected)
