b21986a937d2f501f7c41f40c07acbb67ff43b27
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-11 02:05:06 -0700

    Fix method on fetchDocuments

--- site/src/actions/fetchDocuments.ts
+++ site/src/actions/fetchDocuments.ts
@@ -1,7 +1,7 @@
 import { API } from "../config"
 
 const fetchManyDocuments = async (ids: string[], token: string): Promise<Response> => {
-  return await fetch(API + "/documents", { headers: { "Authorization": token }, body: JSON.stringify({ "documentIds": ids }) })
+  return await fetch(API + "/documents", { method: "post", headers: { "Authorization": token }, body: JSON.stringify({ "documentIds": ids }) })
 }
 
 export default fetchManyDocuments
