eb8d654d77fc897dddf12a38a3ff392ea829b1ad
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-16 00:15:58 -0700

    Add useStatuses hook

--- site/src/hooks/index.ts
+++ site/src/hooks/index.ts
@@ -2,7 +2,8 @@ import useChangelog from "./useChangelog"
 import useClickToggler from "./useClickToggler"
 import useInput from "./useInput"
 import useScrollTo from "./useScrollTo"
+import useStatuses from "./useStatuses"
 import useUserDocuments from "./useUserDocuments"
 import useUserStore from "./useUserStore"
 
-export { useChangelog, useClickToggler, useInput, useScrollTo, useUserDocuments, useUserStore }
+export { useChangelog, useClickToggler, useInput, useScrollTo, useStatuses, useUserDocuments, useUserStore }
