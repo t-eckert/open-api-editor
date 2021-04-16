1787295c30318b8afa7968a7b8975ca4e3c97c11
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-15 23:52:39 -0700

    Implement fetchUserDocuments

--- site/src/actions/fetchDocuments.ts
+++
@@ -1,7 +0,0 @@
-import { API } from "../config"
-
-const fetchManyDocuments = async (ids: string[], token: string): Promise<Response> => {
-  return await fetch(API + "/documents", { method: "post", headers: { "Authorization": token }, body: JSON.stringify({ "documentIds": ids }) })
-}
-
-export default fetchManyDocuments
--- site/src/actions/index.ts
+++ site/src/actions/index.ts
@@ -1,8 +1,8 @@
 import createDocument from "./createDocument"
 import fetchChangelog from "./fetchChangelog"
-import fetchManyDocuments from "./fetchDocuments"
+import fetchUserDocuments from "./fetchUserDocuments"
 import loginUser from "./loginUser"
 import saveDocument from "./saveDocument"
 import submitFeedback from "./submitFeedback"
 
-export { createDocument, fetchChangelog, fetchManyDocuments, loginUser, saveDocument, submitFeedback }
+export { createDocument, fetchChangelog, fetchUserDocuments, loginUser, saveDocument, submitFeedback }
