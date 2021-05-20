9f97e19e9a0c396ace02259aa8ac51060323f5ae
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-05-20 08:37:09 -0700

    Change documents endpoint to graphql

--- api/func_graphql_handler/__init__.py
+++ api/func_graphql_handler/__init__.py
@@ -1,7 +1,7 @@
 from azure.functions import HttpRequest, HttpResponse
-from func_documents_handler.documents_router import documents_router
 from lib.auth import jwt
 from lib.models import User
+from lib.schema import schema
 from lib.sentry import connect_to_sentry, serverless_function
 from typing import Optional
 
@@ -12,7 +12,7 @@ connect_to_sentry()
 
 
 @serverless_function
-def handle_documents_request(request: HttpRequest) -> HttpResponse:
+def handle_graphql_request(request: HttpRequest) -> HttpResponse:
     """Recieves and properly routes the request to the correct handler and returns the result
 
     Args:
@@ -22,7 +22,7 @@ def handle_documents_request(request: HttpRequest) -> HttpResponse:
         HttpResponse:           response to the client
     """
 
-    logging.info(f"/documents recieved a {request.method} request")
+    logging.info(f"/graphql recieved a {request.method} request")
 
     token: Optional[str] = request.headers["Authorization"]
 
@@ -35,6 +35,4 @@ def handle_documents_request(request: HttpRequest) -> HttpResponse:
     except Exception:
         return HttpResponse("Invalid auth token", status_code=401)
 
-    response: HttpResponse = documents_router.route(request.method, request, user_id, token)
-
-    return response or HttpResponse("Could not process request")
+    return schema.execute()
