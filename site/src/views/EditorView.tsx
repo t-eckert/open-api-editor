eb8af4b04e5688bcd5464d71dcf7e94d72cdcdfe
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-05-22 00:50:50 -0700

    Use document in EditorView

--- site/src/views/EditorView.tsx
+++ site/src/views/EditorView.tsx
@@ -1,11 +1,28 @@
+import { observer } from "mobx-react-lite"
+
 import Editor from "../components/editor/Editor"
 import MainLeading from "../components/layout/MainLeading"
 import MainCenter from "../components/layout/MainCenter"
 import MainFollowing from "../components/layout/MainFollowing"
 import Navigation from "../components/editor/Navigation"
 import FeedbackToken from "../components/tokens/FeedbackToken"
+import { useDocument, useDocumentStore, useUserStore } from "../hooks"
 
-const EditorView = () => {
+type Props = {
+  match: { params: { id: string } }
+}
+
+const EditorView = observer((props: Props) => {
+  const documentId = props.match.params.id
+
+  const userStore = useUserStore()
+
+  const token = userStore.jwt || ""
+
+  const { openApiDocument } = useDocument(token, documentId)
+
+  const documentStore = useDocumentStore(openApiDocument)
+
   return (
     <main className="flex flex-col sm:flex-row w-screen">
       <MainLeading>
@@ -14,12 +31,15 @@ const EditorView = () => {
         </div>
       </MainLeading>
       <MainCenter>
-        <Editor></Editor>
+        <pre>
+          <code>{JSON.stringify(documentStore.openApiDocument)}</code>
+        </pre>
+        <Editor />
       </MainCenter>
       <MainFollowing />
       <FeedbackToken className="fixed left-4 bottom-4" />
     </main>
   )
-}
+})
 
 export default EditorView
