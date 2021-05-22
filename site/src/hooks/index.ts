36361a35c28e209db75c34d77d4620330a659418
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-05-21 21:03:49 -0700

    Flesh out the alert store and alert logit

--- site/src/hooks/index.ts
+++ site/src/hooks/index.ts
@@ -1,10 +1,10 @@
+import useAlerts from "./useAlerts"
 import useChangelog from "./useChangelog"
 import useClickToggler from "./useClickToggler"
 import useDocumentList from "./useDocumentList"
 import useInput from "./useInput"
 import useScrollTo from "./useScrollTo"
-import useStatuses from "./useStatuses"
 import useUserStore from "./useUserStore"
 
 
-export { useChangelog, useClickToggler, useDocumentList, useInput, useScrollTo, useStatuses, useUserStore }
+export { useAlerts, useChangelog, useClickToggler, useDocumentList, useInput, useScrollTo, useUserStore }
