79871a2ac17dc8e827ce1fe75c2c0c88955b976f
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-05-26 15:40:34 -0700

    Add changelog query

--- api/lib/schema/query.py
+++ api/lib/schema/query.py
@@ -1,9 +1,17 @@
-from graphene import ObjectType, List
+from graphene import ObjectType, List, String
+import graphene
 from lib.schema.open_api_document import OpenApiDocument
+from lib.models.open_api_document import OpenApiDocument as OpenApiDocumentModel
+from lib.changelog import get_changelog
+from mocks.open_api_document import mock_open_api_documents
 
 
 class Query(ObjectType):
-    open_api_documents = List(OpenApiDocument)
+    open_api_documents = String()
+    changelog = String()
 
     def resolve_open_api_documents(self, info):
-        return list(OpenApiDocument.objects.all())
+        return "Hi"
+
+    def resolve_changelog(self, info):
+        return get_changelog()
